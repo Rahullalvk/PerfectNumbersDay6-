5b44a59e50af93c76fe994a3dfb435f2ad9d1513
Rahulalvk <rahulal432@gmail.com>
2023-05-04 05:41:12 -0700

    Program to find the perfect number

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumber
+{
+    internal class PerfectNumber
+    {
+        public static void DisplayPerfectNumber() 
+        {
+            int n, max, min, sum, i;
+            Console.WriteLine("Enter the Starting value :");
+            min=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Ending value :");
+            max = Convert.ToInt32(Console.ReadLine());
+            for(n=min; n<=max; n++) 
+            
+            
+          {
+                sum = 0;
+                i=1;
+                while (i < n) 
+                {
+                    if(n%i== 0)
+                    sum = sum + i;
+                    i++;
+                }
+                if(sum == n)
+                {
+                    Console.WriteLine("The perfect number is :"+n);
+                }
+            }
+
+        }
+    }
+}
